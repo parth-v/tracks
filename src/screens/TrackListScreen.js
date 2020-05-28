f105615ed741a2cc13ff5bbf9e32d06fe6ccf73d
parth-v <parthvashisht20@gmail.com>
2020-05-29 02:01:19 +0530

    adding initial files

---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const TrackListScreen = () => {
+	return(
+		<View>
+			<Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
