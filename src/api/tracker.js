fd30db217210ba84e6d28d7e126572f15719d6a2
parth-v <parthvashisht20@gmail.com>
2020-05-29 18:06:49 +0530

    server connected to signup

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+	baseURL: 'https://cd898f620f70.ngrok.io'
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,37 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, dispatch) => {
 	switch(action.type) {
 		default: 
 			return state;
-	}
-}
+	};
+};
 
+const signup = (dispatch) => {
+	return async ({ email, password }) => {
+		try {
+			const response = await trackerApi.post('/signup', { email, password });
+			console.log(response.data);
+		} catch (err) {
+			console.log(err.response.data);
+		}
+	};
+};
+
+const signin = (dispatch) => {
+	return ({ email, password }) => {
+
+	};
+};
+
+const signout = (dispatch) => {
+	return () => {
+
+	};
+};
 export const { Context, Provider } = createDataContext(
 	authReducer,
-	{},
+	{ signup, signin, signout},
 	{ isSignedIn: false }
 );
