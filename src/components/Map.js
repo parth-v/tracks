64d91cbec927f31afe5a0b371122187ff73b8494
parth-v <parthvashisht20@gmail.com>
2020-06-04 02:31:03 +0530

    disabled background tracking

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { StyleSheet, ActivityIndicator } from 'react-native';
 import { Text } from 'react-native-elements';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
@@ -19,12 +19,13 @@ const Map = () => {
 				latitudeDelta: 0.01,
 				longitudeDelta: 0.01
 			}}
-			region= {{
-				...currentLocation.coords,
-				latitudeDelta: 0.01,
-				longitudeDelta: 0.01
-			}}
 		>
+			<Circle 
+				center={currentLocation.coords}
+				radius={30}
+				strokeColor="rgba(158, 158, 255, 1)"
+				fillColor="rgba(158, 158, 255, 0.3)"
+			/>
 		</MapView>
 	);
 };
