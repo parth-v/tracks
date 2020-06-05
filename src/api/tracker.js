fccc09ba891638f7ba9b8f8ce3f0bb72e9c990ba
parth-v <parthvashisht20@gmail.com>
2020-06-06 00:42:42 +0530

    track detail completed

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-	baseURL: 'https://baee1883d2ea.ngrok.io'
+	baseURL: 'https://321f1c654c5a.ngrok.io'
 });
 
 instance.interceptors.request.use(
