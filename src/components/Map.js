ca60557acb20fab03586bbe145486fb688bf3b5d
parth-v <parthvashisht20@gmail.com>
2020-06-05 20:39:19 +0530

    added line tracer

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,7 @@ import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
-	const { state: { currentLocation } } = useContext(LocationContext);
+	const { state: { currentLocation, locations } } = useContext(LocationContext);
 
 	if(!currentLocation) {
 		return <ActivityIndicator size='large' style={{ marginTop: 120 }} />
@@ -26,6 +26,7 @@ const Map = () => {
 				strokeColor="rgba(158, 158, 255, 1)"
 				fillColor="rgba(158, 158, 255, 0.3)"
 			/>
+			<Polyline coordinates={locations.map(loc => loc.coords)} />
 		</MapView>
 	);
 };
